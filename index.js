809d82f7d25376050eee41bc254a812c5e512c01
Artrunde <devops@artrunde.com>
2017-02-27 10:29:11 +0100

    Changed for simple test

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-var spawn = require('child_process').spawn
-var stream = require('stream')
+var spawn = require('child_process').spawn;
 
-exports.handler = function( event, context ) {
+exports.handler = function(event, context) {
+	
+    var php = spawn('./php-cgi', ['-v']);
+    var output = '';
+	
+    php.stdout.on('data', function(data) {
+        output += data.toString('utf-8');
+    });
 
-	var php = spawn('./php', ['index.php'])
-  
-	php.stdin.setEncoding = 'utf-8';
-	php.stdin.write(JSON.stringify(event) + "\n")
-	php.stdin.end()
-
-	//php.stdout.pipe(process.stdout);	
-
-	php.stdout.on('data', function (data) {
-		console.log( data.toString() );
-	});
-
-	php.stderr.on('data', function (data) {
-		console.log( data.toString() );
-	});
-
-//var readable = new stream.Readable();
-//readable._read = function noop() {}; // See note below
-//readable.push('test me!');
-//readable.push(null);
-//readable.pipe(php.stdin)
-
-	php.on('exit', function (code) {
-  		console.log('child process exited with code ' + code);
-		context.done()
-	});
-
-}
+    php.on('close', function() {
+        context.succeed(output);
+    });
+	
+};
