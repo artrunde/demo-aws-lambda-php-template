d6628201bd76e2bf59daa36b08ab433b8b428bd7
Karsten JAkobsen <karsten@karstenjakobsen.dk>
2017-02-28 14:48:47 +0000

    Lambda PHP

--- index.js
+++ index.js
@@ -12,7 +12,29 @@ exports.handler = function(event, context) {
 
   console.log("request: " + JSON.stringify(event));
 
-  var php = spawn('./bin/php-cgi', ['-v']);
+  // Sets some sane defaults here so that this function doesn't fail when it's not handling a HTTP request from
+  // API Gateway.
+  var requestMethod = event.httpMethod || 'GET';
+  var serverName = event.headers ? event.headers.Host : '';
+  var requestUri = event.path || '';
+
+  // Spawn the PHP CGI process with a bunch of environment variables that describe the request.
+  var php = spawn('./bin/php-cgi', ['index.php'], {
+    env: Object.assign({
+      REDIRECT_STATUS: 200,
+      REQUEST_METHOD: requestMethod,
+      SCRIPT_FILENAME: 'index.php',
+      SCRIPT_NAME: '/index.php',
+      PATH_INFO: '/',
+      SERVER_NAME: serverName,
+      SERVER_PROTOCOL: 'HTTP/1.1',
+      REQUEST_URI: requestUri
+    })
+  });
+
+  //send the input event json as string via STDIN to php process
+  php.stdin.write(event.body);
+  php.stdin.end(); // Close the php stream to unblock php process
 
   php.stdout.on('data', function(data) {
     PHPOutput += data.toString('utf-8');
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
+$inputJSON = file_get_contents('php://stdin');
+
+var_dump($inputJSON);
 var_dump($_SERVER);
